1d4c70fe68ebdd98af74728c0388bfdf8c51ce00
Ovidiu <stanciu.ovidiu99@yahoo.com>
2021-11-14 14:25:24 +0200

    Added Maven & created main class "Server"

---
+++ src/main/java/web/server/Server.java
@@ -0,0 +1,13 @@
+package web.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+    private final static Logger LOGGER =  LoggerFactory.getLogger(Server.class);
+    public static void main(String[] args) {
+        System.out.println("Server started ...");
+    }
+
+
+}
